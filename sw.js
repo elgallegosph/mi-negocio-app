eccaa336e88310916966301dcf35fc045bd0bbd4
elgallegosph <mateosax1998@gmail.com>
2026-04-09 23:09:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'negocio-v1';
+const CACHE_NAME = 'negocio-v2';
 const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
