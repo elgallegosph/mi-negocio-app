83edce47db09c7ace9dfc58d38d24aff71cec305
elgallegosph <mateosax1998@gmail.com>
2026-04-09 22:08:50 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+const CACHE_NAME = 'v1_cache_negocio';
+const urlsToCache = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
