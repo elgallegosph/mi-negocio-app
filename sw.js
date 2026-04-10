5b558a87d695b6b2f8fa3f89721218b1c8514c20
elgallegosph <mateosax1998@gmail.com>
2026-04-09 23:00:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'v1_cache_negocio';
-const urlsToCache = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+const CACHE_NAME = 'negocio-v1';
+const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
